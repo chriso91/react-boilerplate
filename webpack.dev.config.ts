713a269db5493166cb0f4efb67a99b52c470a05f
chriso91 <c.e.odea@gmail.com>
2021-10-28 09:47:58 -0400

    chore: working with docker problems

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import { Configuration, HotModuleReplacementPlugin } from "webpack";
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 export const config: Configuration = {
   mode: 'development',
@@ -30,9 +29,9 @@ export const config: Configuration = {
              use: [
                // Creates `style` nodes from JS strings
                { loader: "style-loader" },
-               { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+               { loader: "css-modules-typescript-loader"},  
                // Translates CSS into CommonJS
-               { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+               { loader: "css-loader", options: { modules: true } },
                // Compiles Sass to CSS
                { loader: "sass-loader" },
              ],
@@ -57,7 +56,6 @@ export const config: Configuration = {
         template: "src/index.html",
       }),
       new HotModuleReplacementPlugin(),
-      new MiniCssExtractPlugin()
     ],
 };
 
